8ad81b67164969dd2b351adb59a7e5370bb4bb76
hquyen <haquangquyen12345@gmail.com>
2026-05-07 00:55:00 +0700

    Update wifi_app.h

--- wifi_app.h
+++ wifi_app.h
@@ -3,12 +3,11 @@
 
 #include "esp_wifi.h"
 #include "esp_event.h"
-
+#include <stdbool.h>
 // Thay đổi tên mạng và mật khẩu nhà bạn ở đây
 #define WIFI_SSID       "quyendeptraivl"
 #define WIFI_PASS       "cccccccc"
-#define MAXIMUM_RETRY   5 // Số lần kết nối lại tối đa nếu bị rớt mạng
-
+extern bool is_wifi_connected;
 void wifi_init_sta(void);
 
 #endif
